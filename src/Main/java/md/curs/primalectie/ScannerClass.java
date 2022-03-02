58d86823d167bfc8ac105cf842063e92769a1085
Expert-26 <andrei.fitcoach@gmail.com>
2022-03-02 21:05:10 +0200

    small changes

--- src/Main/java/md/curs/primalectie/ScannerClass.java
+++ src/Main/java/md/curs/primalectie/ScannerClass.java
@@ -7,7 +7,7 @@ public class ScannerClass {
     public static void main(String args[]) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("What is your name: ");
+        System.out.println("Whaaat is your name : ");
         String name = scanner.nextLine();
         System.out.println("Hello " + name);
 
