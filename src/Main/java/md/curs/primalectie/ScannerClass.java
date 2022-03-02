6d404a68b3f8a6c80811916c9785025a8459f5bf
Expert-26 <andrei.fitcoach@gmail.com>
2022-03-02 21:06:18 +0200

    removed a

--- src/Main/java/md/curs/primalectie/ScannerClass.java
+++ src/Main/java/md/curs/primalectie/ScannerClass.java
@@ -7,7 +7,7 @@ public class ScannerClass {
     public static void main(String args[]) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Whaaat is your name : ");
+        System.out.println("Whaat is your name : ");
         String name = scanner.nextLine();
         System.out.println("Hello " + name);
 
